af2f13820d1824d7a4fa30e3df1236d944724464
James Sun <james.b.sun@gmail.com>
2016-12-07 19:25:20 -0800

    Adding utils file

---
+++ utils.py
@@ -0,0 +1,12 @@
+import random
+
+
+def weighted_random_choice(choices):
+    total = sum(w for c, w in choices)
+    r = random.uniform(0, total)
+    upto = 0
+    for c, w in choices:
+        if upto + w >= r:
+            return c
+        upto += w
+    raise Exception("Error in Weighted Random Choice")
